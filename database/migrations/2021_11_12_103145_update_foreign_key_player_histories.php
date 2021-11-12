9e58c52c596cbdc591ccc11064bc7e1c421607ec
Mardy A. dela Cruz <mardy.codev@gmail.com>
2021-11-12 18:47:45 +0800

    remove cascade on foreign key relation

---
+++ database/migrations/2021_11_12_103145_update_foreign_key_player_histories.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateForeignKeyPlayerHistories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('player_scholar_histories', function (Blueprint $table) {
+            $table->dropForeign('player_scholar_histories_player_id_foreign');
+            $table->dropForeign('player_scholar_histories_scholar_id_foreign');
+
+            $table->foreign('scholar_id')->nullable()
+                ->references('id')->on('scholars');
+            $table->foreign('player_id')->nullable()
+                ->references('id')->on('players');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('player_scholar_histories', function (Blueprint $table) {
+            $table->dropForeign('player_scholar_histories_player_id_foreign');
+            $table->dropForeign('player_scholar_histories_scholar_id_foreign');
+
+            $table->foreign('scholar_id')->nullable()
+                ->references('id')->on('scholars');
+            $table->foreign('player_id')->nullable()
+                ->references('id')->on('players');
+        });
+    }
+}
