f4a07bd06da8257530691534e48673b74fb675f0
Mardy A. dela Cruz <mardy.codev@gmail.com>
2021-11-16 19:57:45 +0800

    Initial commit for email

---
+++ resources/views/emails/payroll_reminder.blade.php
@@ -0,0 +1,5 @@
+Dear {{ $user->first_name }} <br><br>
+
+I want to notify you about the payroll.
+
+Note: this is a sample email for project development.
---
+++ resources/views/emails/slp_update.blade.php
@@ -0,0 +1,5 @@
+Dear {{ $user->first_name }} <br><br>
+
+SLP Update
+
+Note: this is a sample email for project development.
