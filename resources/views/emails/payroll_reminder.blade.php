de7fe3901dd41b6ce583d03d221852e189a22bf7
Mardy A. dela Cruz <mardy.codev@gmail.com>
2021-11-18 14:28:28 +0800

    add breakline

--- resources/views/emails/payroll_reminder.blade.php
+++ resources/views/emails/payroll_reminder.blade.php
@@ -1,5 +1,5 @@
 Dear {{ $user->first_name ?? 'Scholar' }} <br><br>
 
-I want to notify you about the payroll.
-
+I want to notify you about the payroll. <br>
+<br>
 Note: this is a sample email for project development.
--- resources/views/emails/slp_update.blade.php
+++ resources/views/emails/slp_update.blade.php
@@ -1,5 +1,5 @@
 Dear {{ $user->first_name ?? 'Scholar' }} <br><br>
 
-SLP Price : PHP{{ $slp_value }}
-
+SLP Price : PHP{{ $slp_value }} <br>
+<br>
 Note: this is a sample email for project development.
