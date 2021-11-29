7630d416ccbe5430fe19b0978e086fdce560617b
Mardy A. dela Cruz <mardy.codev@gmail.com>
2021-11-29 15:11:55 +0800

    SLP Update using target price initial update

--- app/Console/Commands/SlpUpdate.php
+++ app/Console/Commands/SlpUpdate.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\NotificationSettings;
 use App\Models\Scholar;
+use App\Models\SlpPriceNotificationHistory;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Model;
@@ -56,11 +57,6 @@ class SlpUpdate extends Command
 //
 //        $this->line('Sending to : ' . $scholar_emails);
 
-        //TODO : check if already sent this day
-
-        //slp_price_notifications_history
-        //datetime, price,
-        //
 
         $response = Http::get('https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=php,jpy,usd');
         if ($response->failed()) {
@@ -79,20 +75,36 @@ class SlpUpdate extends Command
 
         $this->line('Options: ' . print_r($notif_options, true));
 
-        if ($notif_options && $slp_prices[strtolower($notif_options['target_slp_unit'])] >= $notif_options['target_slp_price']) {
+        $currency = $notif_options['target_slp_unit'];
+        $slp_value = $slp_prices[strtolower($currency)];
+        $target_slp_price = $notif_options['target_slp_price'] ?? null;
+
+        $latestSlpNotificationToday = SlpPriceNotificationHistory::whereDate('sent_at', '=', Carbon::now()->format('Y-m-d'))->latest('sent_at')->first();
+        $this->line($latestSlpNotificationToday ?? 'No presnt notification');
+        if ($latestSlpNotificationToday && $target_slp_price == $latestSlpNotificationToday->value && $currency == $latestSlpNotificationToday->currency) {
+            $this->line('Already sent this notification');
+            $this->line('SLP Update End: ' . Carbon::now()->format('Y-m-d H:i:s'));
+            return 0;
+        }
+
+        if ($notif_options && $slp_value >= $notif_options['target_slp_price']) {
+
+
             $this->line('Will send email notif');
-            //temporary for testing cron job
-//        $scholar_emails = ['mhardz07@gmail.com','elfredtapar@gmail.com'];
-//            Mail::to($scholar_emails)->send(new \App\Mail\SlpUpdate(null, $slp_price));
 
             //testing only for now
             $scholars = [new Scholar(['email' => 'mhardz07@gmail.com', 'first_name' => 'Mardy']), new Scholar(['email' => 'elfredtapar@gmail.com', 'first_name' => 'Elfred'])];
 
             if ($scholars) {
                 foreach ($scholars as $scholar) {
-                    Mail::to($scholar->email)->send(new \App\Mail\SlpUpdate($scholar, $slp_prices[strtolower($notif_options['target_slp_unit'])], $notif_options['target_slp_unit']));
+                    Mail::to($scholar->email)->send(new \App\Mail\SlpUpdate($scholar, $slp_value, $currency));
                 }
             }
+
+            SlpPriceNotificationHistory::create([
+                'value' => $target_slp_price,
+                'currency' => $currency
+            ]);
         }
 
 
