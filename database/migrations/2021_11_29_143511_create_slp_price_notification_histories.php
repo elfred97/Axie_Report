57b1ce54dd6ea5449f3174c6fa2e4d4e38cdd6f2
GlenWinWin <glenwinbernabe@gmail.com>
2021-12-15 12:02:58 +0800

    update column value to be nullable in slppricenotificationHistory

--- database/migrations/2021_11_29_143511_create_slp_price_notification_histories.php
+++ database/migrations/2021_11_29_143511_create_slp_price_notification_histories.php
@@ -15,7 +15,7 @@ class CreateSlpPriceNotificationHistories extends Migration
     {
         Schema::create('slp_price_notification_histories', function (Blueprint $table) {
             $table->id();
-            $table->decimal('value',20,10);
+            $table->decimal('value',20,10)->nullable();
             $table->string('currency');
             $table->timestamp('sent_at')->useCurrent();
         });
