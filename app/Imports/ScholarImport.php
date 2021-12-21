1000857abbdb339e9235ab0e5742aa1f711cd117
GlenWinWin <glenwinbernabe@gmail.com>
2021-12-21 09:17:14 +0800

    update

--- app/Imports/ScholarImport.php
+++ app/Imports/ScholarImport.php
@@ -25,17 +25,17 @@ class ScholarImport implements ToCollection,WithHeadingRow,WithValidation,SkipsO
     {
         foreach ($rows as $row)
         {
-                Scholar::create([
-                    'first_name'   => $row['first_name'],
-                    'middle_name'  => $row['middle_name'],
-                    'last_name'    => $row['last_name'],
-                    'email'        => $row['email'],
-                    'username'     => $row['username'],
-                    'password'     => bcrypt($row['password']),
-                    'date_started' => date('Y-m-d H:i:s' , strtotime($row['date_started'])),
-                    'type'      => $row['type'],
-                    'status'       => $row['status'],
-                ]);
+            Scholar::create([
+                'first_name'   => $row['first_name'],
+                'middle_name'  => $row['middle_name'],
+                'last_name'    => $row['last_name'],
+                'email'        => $row['email'],
+                'username'     => $row['username'],
+                'password'     => bcrypt($row['password']),
+                'date_started' => date('Y-m-d H:i:s' , strtotime($row['date_started'])),
+                'type'      => $row['type'],
+                'status'       => $row['status'],
+            ]);
         }
     }
     public function rules(): array
