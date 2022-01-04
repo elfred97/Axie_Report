20921ce401f70849f2d09a428522711aa1aced26
GlenWinWin <glenwinbernabe@gmail.com>
2022-01-04 14:30:05 +0800

    Added function for battle_logs and update table battle_logs

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -172,10 +172,10 @@ class FileController extends Controller
             $from = Carbon::parse('01-01-2020');
             $to   = Carbon::now();
 
-            if($request->date){
+            //if($request->date){
                 $from = ($request->date[0]) ? Carbon::parse($request->date[0]) : $from;
                 $to   = ($request->date[1]) ? Carbon::parse($request->date[1]) : $to;
-            }
+            //}
             $notification = DB::TABLE('notification as n')
                 ->SELECT('n.*', DB::RAW('concat(s.first_name," ",s.last_name) as player_name'), 'r.gained_slp_today', 'r.mmr')
                 ->LEFTJOIN('players as p', 'p.account_name', '=', 'n.account_name')
