84680e6b51f1ee8e4541544fdcf5f60980f257e9
GlenWinWin <glenwinbernabe@gmail.com>
2022-01-04 14:32:04 +0800

    Register cronjob for battle_logs in Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('slp-update')->everyMinute()->runInBackground()->withoutOverlapping();
         $schedule->command('remind-payroll')->monthlyOn(10, '08:00')->runInBackground()->withoutOverlapping();
-        $schedule->command('reminders:check-and-send')->everyMinute()->runInBackground()->withoutOverlapping(); //needs to check reminder schedule so need to run every minute
+        $schedule->command('reminders:check-and-send')->everyMinute()->runInBackground()->withoutOverlapping();
+        $schedule->command('battle.logs')->dailyAt('8:00')->runInBackground()->withoutOverlapping(); //needs to check reminder schedule so need to run every minute
     }
 
     /**
