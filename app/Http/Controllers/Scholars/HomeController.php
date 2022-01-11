0febbd270b6b7c013a8d15e41572b6f531cc0696
GlenWinWin <glenwinbernabe@gmail.com>
2022-01-11 12:22:01 +0800

    update function for getScholarNotification

--- app/Http/Controllers/Scholars/HomeController.php
+++ app/Http/Controllers/Scholars/HomeController.php
@@ -315,7 +315,7 @@ class HomeController extends Controller
                 DB::RAW('CONCAT(scholars.first_name, " ", scholars.last_name) as scholar_name'),
                 'players.*'
             )
-        ->WHERE([['scholars.username', $username], ['notification.category', '!=', 3]])
+        ->WHERE([['scholars.username', $username]])
         ->GET();
 
     }
