6b07ffcaf34a89373d335f40d15215de9f702a01
GlenWinWin <glenwinbernabe@gmail.com>
2022-01-11 17:41:54 +0800

    Update getNotification in admin

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -160,7 +160,7 @@ class FileController extends Controller
         $from = Carbon::parse('01-01-2020');
         $to   = Carbon::now();
 
-        if($request->date != 'today'){
+        if($request->date){
             $from = ($request->date[0]) ? Carbon::parse($request->date[0]) : $from;
             $to   = ($request->date[1]) ? Carbon::parse($request->date[1]) : $to;
         }
