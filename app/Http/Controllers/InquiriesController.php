eacaf5f674591d7304833b366630c3279190b266
GlenWinWin <glenwinbernabe@gmail.com>
2022-01-19 15:59:02 +0800

    update InquiriesController.php

--- app/Http/Controllers/InquiriesController.php
+++ app/Http/Controllers/InquiriesController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Mail;
 class InquiriesController extends Controller
 {
     public function send(Request $request){
-        Mail::to('temporary_scholar@test.admin.ventures')->send(new \App\Mail\Inquiries($request));
+        Mail::to('noreplytest@admin.ventures')->send(new \App\Mail\Inquiries($request));
         return 'Email Sent';
     }
 }
