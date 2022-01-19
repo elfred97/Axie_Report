8807267776a9a4c541c193a917fa8f31f6a39412
elfred97 <elfredtapar@gmail.com>
2022-01-19 15:25:28 +0800

    Design for documentation

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -5,5 +5,11 @@
     </header>
     <body>
         <h1>Help</h1>
+        <form action="send_inquiries" method="POST">
+            <input type="email" name="email" id="email">
+            <input type="text" name="subject" id="subject">
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+            <button type="submit">Send</button>
+        </form>
     </body>
 </html>
